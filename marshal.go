5f6f7389e9c7dc2c7bcf2eab0d690105fbd17c88
pcouto <phillip.couto@stemstudios.com>
2013-08-19 09:15:09 -0400

    fixed go formatting.

--- marshal.go
+++ marshal.go
@@ -336,12 +336,11 @@ func marshalBigInt(info *TypeInfo, value interface{}) ([]byte, error) {
 	case int:
 		return encBigInt(int64(v)), nil
 	case uint:
-	if(v > math.MaxUint32)
-	{
-		if(v > math.MaxInt64) {
-			return nil, marshalErrorf("marshal bigint: value %d out of range", v)
+		if v > math.MaxUint32 {
+			if v > math.MaxInt64 {
+				return nil, marshalErrorf("marshal bigint: value %d out of range", v)
+			}
 		}
-	}
 		return encBigInt(int64(v)), nil
 	case int64:
 		return encBigInt(v), nil
