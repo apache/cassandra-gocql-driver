2a31910b3715a33d83e830c05a8b46b57557ed43
Ben Hood <0x6e6562@gmail.com>
2014-04-30 17:34:53 +0100

    Wait for the server to start before running test

--- integration.sh
+++ integration.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
 
-PID_FILE=cassandra.pid
+set -e
 
-for v in 2.0.7
+PID_FILE=cassandra.pid
+STARTUP_LOG=startup.log
+ARCHIVE_BASE_URL=http://archive.apache.org/dist/cassandra
+
+for v in 2.0.6 2.0.7
 do
 	TARBALL=apache-cassandra-$v-bin.tar.gz
 	CASSANDRA_DIR=apache-cassandra-$v
 
-	curl -L -O -C - ftp://ftp.mirrorservice.org/sites/ftp.apache.org/cassandra/$v/$TARBALL
+	curl -L -O -C - $ARCHIVE_BASE_URL/$v/$TARBALL
 	
 	if [ ! -f $CASSANDRA_DIR/bin/cassandra ]
 	then
-   		tar xzvf $TARBALL
+   		tar xzf $TARBALL
 	fi
+	
+	CASSANDRA_LOG_DIR=`pwd`/v${v}/log/cassandra
+	CASSANDRA_LOG=$CASSANDRA_LOG_DIR/system.log
 
-	echo "Booting Cassandra ${v}"
+	mkdir -p $CASSANDRA_LOG_DIR
+	: >$CASSANDRA_LOG  # create an empty log file
+	
+	sed -i -e 's?/var?'`pwd`/v${v}'?' $CASSANDRA_DIR/conf/cassandra.yaml
+	sed -i -e 's?/var?'`pwd`/v${v}'?' $CASSANDRA_DIR/conf/log4j-server.properties
 
-	apache-cassandra-2.0.7/bin/cassandra -p $PID_FILE > /dev/null 2>&1
-	read PID <$PID_FILE
+	echo "Booting Cassandra ${v}, waiting for CQL listener to start ...."
 
-	echo "Cassandra ${v} running (PID ${PID})"
+	$CASSANDRA_DIR/bin/cassandra -p $PID_FILE &> $STARTUP_LOG
+
+	{ tail -n +1 -f $CASSANDRA_LOG & } | sed -n '/Starting listening for CQL clients/q'
+	
+	PID=$(<"$PID_FILE")
+
+	echo "Cassandra ${v} running (PID ${PID}), about to run test suite ...."
 
 	go test -v ./...
 
-	echo "Killing Cassandra ${v} (PID ${PID})"
+	echo "Test suite passed against Cassandra ${v}, killing server instance (PID ${PID})"
 	
 	kill -9 $PID
 	rm $PID_FILE
