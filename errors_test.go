0e247a0f39ffc900327a9b2b1f68cb5e076da0e3
Nimi Wariboko Jr <nimi@channelmeter.com>
2014-04-30 22:15:05 -0700

    Added test to ensure library can properly decode error response from cassandra

---
+++ errors_test.go
@@ -0,0 +1,27 @@
+package gocql
+
+import (
+	"testing"
+)
+
+func TestErrorsParse(t *testing.T) {
+	session := createSession(t)
+	defer session.Close()
+
+	if err := session.Query(`CREATE TABLE errors_parse (id int primary key)`).Exec(); err != nil {
+		t.Fatal("create:", err)
+	}
+
+	if err := session.Query(`CREATE TABLE errors_parse (id int primary key)`).Exec(); err == nil {
+		t.Fatal("Should have gotten already exists error from cassandra server.")
+	} else {
+		switch e := err.(type) {
+		case errRespAlreadyExists:
+			if e.Table != "errors_parse" {
+				t.Fatal("Failed to parse error response from cassandra for ErrAlreadyExists.")
+			}
+		default:
+			t.Fatal("Failed to parse error response from cassandra for ErrAlreadyExists.")
+		}
+	}
+}
