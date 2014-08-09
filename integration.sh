685061926838dd9d70cf84c8eb4c43ed82005423
Chris Bannister <c.bannister@gmail.com>
2014-08-09 16:00:02 +0100

    Remove disable auto compaction for now

--- integration.sh
+++ integration.sh
@@ -7,10 +7,6 @@ function run_tests() {
 	ccm create test -v $version -n 3 -s -d --vnodes
 	ccm status
 	ccm updateconf 'concurrent_reads: 8' 'concurrent_writes: 32' 'rpc_server_type: sync' 'rpc_min_threads: 2' 'rpc_max_threads: 8' 'write_request_timeout_in_ms: 5000' 'read_request_timeout_in_ms: 5000'
- 
-	ccm node1 nodetool disableautocompaction
-	ccm node2 nodetool disableautocompaction
-	ccm node3 nodetool disableautocompaction
 
 	local proto=2
 	if [[ $version == 1.2.* ]]; then
