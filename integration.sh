c9f797ecef41ede5c881d449b65a5f192dc4de58
Chris Bannister <c.bannister@gmail.com>
2014-08-09 13:43:42 +0100

    Update ccm and test run args

--- integration.sh
+++ integration.sh
@@ -4,17 +4,16 @@ set -e
 
 function run_tests() {
 	local version=$1
-	ccm create test -v $version -n 3 -s --debug
+	ccm create test -v $version -n 3 -s -d --vnodes
 
 	ccm status
 
+	local proto=2
 	if [[ $version == 1.2.* ]]; then
-		echo "running protocol 1 test suite"
-		go test -v ./... -proto 1
-	else
-		echo "running protocol 2 test suite"
-		go test -v ./...
+		proto=1
 	fi
+
+	go test -v -proto=$proto -rf=3 -cluster=$(ccm liveset) ./...
 
 	ccm clear
 }
