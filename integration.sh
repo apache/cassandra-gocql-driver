0acbc741f576903ea07f666439e0f7b27bb92d56
Chris Bannister <c.bannister@gmail.com>
2014-08-09 14:02:22 +0100

    Tune config down a little

--- integration.sh
+++ integration.sh
@@ -5,8 +5,8 @@ set -e
 function run_tests() {
 	local version=$1
 	ccm create test -v $version -n 3 -s -d --vnodes
-
 	ccm status
+	ccm updateconf 'concurrent_reads: 8' 'concurrent_writes: 8' 'rpc_server_type: hsha' 'rpc_min_threads: 2' 'rpc_max_threads: 8' 'write_request_timeout_in_ms: 5000'
 
 	local proto=2
 	if [[ $version == 1.2.* ]]; then
