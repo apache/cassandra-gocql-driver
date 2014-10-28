583d77761469be9dab8e2aa40a8f0c6590667768
Ben Hood <0x6e6562@gmail.com>
2014-10-28 15:21:49 +0000

    Verbose output for ccm

--- integration.sh
+++ integration.sh
@@ -12,7 +12,7 @@ function run_tests() {
 	sed -i '/#HEAP_NEWSIZE/c\HEAP_NEWSIZE="100M"' ~/.ccm/repository/$version/conf/cassandra-env.sh
 
 	ccm updateconf 'client_encryption_options.enabled: true' 'client_encryption_options.keystore: testdata/pki/.keystore' 'client_encryption_options.keystore_password: cassandra' 'client_encryption_options.require_client_auth: true' 'client_encryption_options.truststore: testdata/pki/.truststore' 'client_encryption_options.truststore_password: cassandra' 'concurrent_reads: 2' 'concurrent_writes: 2' 'rpc_server_type: sync' 'rpc_min_threads: 2' 'rpc_max_threads: 2' 'write_request_timeout_in_ms: 5000' 'read_request_timeout_in_ms: 5000'
-	ccm start
+	ccm start -v
 	ccm status
 
 	local proto=2
