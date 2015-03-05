70c5f8759bbcf776c46b0ec989ad694f285b96de
Chris Bannister <c.bannister@gmail.com>
2015-03-05 08:46:12 +0000

    tweak down the config

--- integration.sh
+++ integration.sh
@@ -8,10 +8,31 @@ function run_tests() {
 
 	ccm create test -v binary:$version -n $clusterSize -d --vnodes
 	
-	sed -i '/#MAX_HEAP_SIZE/c\MAX_HEAP_SIZE="512M"' ~/.ccm/repository/$version/conf/cassandra-env.sh
-	sed -i '/#HEAP_NEWSIZE/c\HEAP_NEWSIZE="100M"' ~/.ccm/repository/$version/conf/cassandra-env.sh
+	sed -i '/#MAX_HEAP_SIZE/c\MAX_HEAP_SIZE="128M"' ~/.ccm/repository/$version/conf/cassandra-env.sh
+	sed -i '/#HEAP_NEWSIZE/c\HEAP_NEWSIZE="32M"' ~/.ccm/repository/$version/conf/cassandra-env.sh
 
-	ccm updateconf 'client_encryption_options.enabled: true' 'client_encryption_options.keystore: testdata/pki/.keystore' 'client_encryption_options.keystore_password: cassandra' 'client_encryption_options.require_client_auth: true' 'client_encryption_options.truststore: testdata/pki/.truststore' 'client_encryption_options.truststore_password: cassandra' 'concurrent_reads: 2' 'concurrent_writes: 2' 'rpc_server_type: hsha' 'rpc_min_threads: 1' 'rpc_max_threads: 8' 'write_request_timeout_in_ms: 5000' 'read_request_timeout_in_ms: 5000'
+	local conf=(
+		'client_encryption_options.enabled: true'
+		'client_encryption_options.keystore: testdata/pki/.keystore'
+		'client_encryption_options.keystore_password: cassandra'
+		'client_encryption_options.require_client_auth: true'
+		'client_encryption_options.truststore: testdata/pki/.truststore'
+		'client_encryption_options.truststore_password: cassandra'
+		'concurrent_reads: 2'
+		'concurrent_writes: 2'
+		'rpc_server_type: hsha'
+		'rpc_min_threads: 1'
+		'rpc_max_threads: 1'
+		'write_request_timeout_in_ms: 5000'
+		'read_request_timeout_in_ms: 5000'
+		'compaction_throughput_mb_per_sec: 0'
+		'in_memory_compaction_limit_in_mb: 1'
+		'reduce_cache_sizes_at: 0'
+		'reduce_cache_capacity_to: 0'
+
+	)
+
+	ccm updateconf ${conf[@]}
 	ccm start -v
 	ccm status
 	ccm node1 nodetool status
