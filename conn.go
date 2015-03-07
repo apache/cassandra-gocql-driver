1bd96c7e0396022ed14337b62ba216415eaf8872
Chris Bannister <c.bannister@gmail.com>
2015-03-07 12:51:32 +0000

    tidy up consts

--- conn.go
+++ conn.go
@@ -21,9 +21,11 @@ import (
 	"time"
 )
 
-const defaultFrameSize = 4096
-const flagResponse = 0x80
-const maskVersion = 0x7F
+const (
+	defaultFrameSize = 4096
+	flagResponse     = 0x80
+	maskVersion      = 0x7F
+)
 
 //JoinHostPort is a utility to return a address string that can be used
 //gocql.Conn to form a connection with a host.
