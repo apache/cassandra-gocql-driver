54d78ae384965e958a6b8538d230acc904127407
Ben Hood <0x6e6562@gmail.com>
2015-03-12 00:02:26 +1000

    Run the auth tests in a separate matrix config

--- integration.sh
+++ integration.sh
@@ -2,39 +2,10 @@
 
 set -e
 
-function compare() {
-    if [[ $1 == $2 ]]
-    then
-        echo 0
-    fi
-    local IFS=.
-    local i ver1=($1) ver2=($2)
-    # fill empty fields in ver1 with zeros
-    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
-    do
-        ver1[i]=0
-    done
-    for ((i=0; i<${#ver1[@]}; i++))
-    do
-        if [[ -z ${ver2[i]} ]]
-        then
-            # fill empty fields in ver2 with zeros
-            ver2[i]=0
-        fi
-        if ((10#${ver1[i]} > 10#${ver2[i]}))
-        then
-            echo 1
-        fi
-        if ((10#${ver1[i]} < 10#${ver2[i]}))
-        then
-            echo 2
-        fi
-    done
-}
-
 function run_tests() {
 	local clusterSize=3
 	local version=$1
+	local auth=$2
 
 	ccm create test -v binary:$version -n $clusterSize -d --vnodes
 
@@ -43,60 +14,47 @@ function run_tests() {
 
 	ccm updateconf 'client_encryption_options.enabled: true' 'client_encryption_options.keystore: testdata/pki/.keystore' 'client_encryption_options.keystore_password: cassandra' 'client_encryption_options.require_client_auth: true' 'client_encryption_options.truststore: testdata/pki/.truststore' 'client_encryption_options.truststore_password: cassandra' 'concurrent_reads: 2' 'concurrent_writes: 2' 'rpc_server_type: sync' 'rpc_min_threads: 2' 'rpc_max_threads: 2' 'write_request_timeout_in_ms: 5000' 'read_request_timeout_in_ms: 5000'
 
+	if [ "$auth" = true ]
+    then
+		ccm updateconf 'authenticator: PasswordAuthenticator' 'authorizer: CassandraAuthorizer'
+	fi
+
 	ccm start -v
 	ccm status
 	ccm node1 nodetool status
+	ccm node2 nodetool status
+	ccm node3 nodetool status
 
 	local proto=2
 	if [[ $version == 1.2.* ]]; then
 		proto=1
 	fi
 
-	go test -timeout 5m -tags integration -cover -v -runssl -proto=$proto -rf=3 -cluster=$(ccm liveset) -clusterSize=$clusterSize -autowait=2000ms ./... | tee results.txt
-
-	if [ ${PIPESTATUS[0]} -ne 0 ]; then
-		echo "--- FAIL: ccm status follows:"
-		ccm status
-		ccm node1 nodetool status
-		ccm node1 showlog > status.log
-		cat status.log
-		echo "--- FAIL: Received a non-zero exit code from the go test execution, please investigate this"
-		exit 1
-	fi
-
-	cover=`cat results.txt | grep coverage: | grep -o "[0-9]\{1,3\}" | head -n 1`
-
-	if [[ $cover -lt "55" ]]; then
-		echo "--- FAIL: expected coverage of at least 60 %, but coverage was $cover %"
-		exit 1
-	fi
-
-	local mimimum=2.1
-	local comp=$(compare $version $mimimum) || true
-
-	echo "Result of version comparison: $comp"
-
-    if [[ $comp == 1 ]]
+	if [ "$auth" = true ]
     then
-    	ccm stop
-		ccm status
+    	go test -v . -timeout 15s -run=TestAuthentication -tags integration -runauth -rf=3 -proto=$proto -cluster=$(ccm liveset) -clusterSize=$clusterSize -autowait=1000ms
+	else
+		go test -timeout 5m -tags integration -cover -v -runssl -proto=$proto -rf=3 -cluster=$(ccm liveset) -clusterSize=$clusterSize -autowait=2000ms ./... | tee results.txt
 
-    	ccm updateconf 'authenticator: PasswordAuthenticator' 'authorizer: CassandraAuthorizer'
+		if [ ${PIPESTATUS[0]} -ne 0 ]; then
+			echo "--- FAIL: ccm status follows:"
+			ccm status
+			ccm node1 nodetool status
+			ccm node1 showlog > status.log
+			cat status.log
+			echo "--- FAIL: Received a non-zero exit code from the go test execution, please investigate this"
+			exit 1
+		fi
 
-		ccm start -v
-		ccm node1 compact
-		ccm node2 compact
-		ccm node3 compact
-		ccm status
-		ccm node1 nodetool status
-		ccm node2 nodetool status
-		ccm node3 nodetool status
+		cover=`cat results.txt | grep coverage: | grep -o "[0-9]\{1,3\}" | head -n 1`
 
-		go test -v . -timeout 15s -run=TestAuthentication -tags integration -runauth -proto=$proto -cluster=$(ccm liveset) -clusterSize=$clusterSize -autowait=5000ms
-    else
-        echo "Ignoring authentication test for Cassandra version $version; mimimum was $mimimum"
-    fi
+		if [[ $cover -lt "55" ]]; then
+			echo "--- FAIL: expected coverage of at least 60 %, but coverage was $cover %"
+			exit 1
+		fi
+	fi
 
 	ccm clear
 }
-run_tests $1
+
+run_tests $1 $2
