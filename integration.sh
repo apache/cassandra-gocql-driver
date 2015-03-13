29000e12f44881f0aada210cbd0a16ce3b4b6d07
Chris Bannister <c.bannister@gmail.com>
2015-03-13 11:58:31 +0000

    ssl is not compatible with hsha, reduce threads, increase phi_threshold

--- integration.sh
+++ integration.sh
@@ -9,8 +9,8 @@ function run_tests() {
 	ccm create test -v binary:$version -n $clusterSize -d --vnodes
 
 	ccm updateconf 'client_encryption_options.enabled: true' 'client_encryption_options.keystore: testdata/pki/.keystore' 'client_encryption_options.keystore_password: cassandra' 'client_encryption_options.require_client_auth: true' 'client_encryption_options.truststore: testdata/pki/.truststore' 'client_encryption_options.truststore_password: cassandra' 'concurrent_reads: 2' 'concurrent_writes: 2' 'rpc_server_type: sync' 'rpc_min_threads: 2' 'rpc_max_threads: 2' 'write_request_timeout_in_ms: 5000' 'read_request_timeout_in_ms: 5000'
-	ccm updateconf 'rpc_server_type: hsha'
-	ccm updateconf 'rpc_min_threads: 8' 'rpc_max_threads: 16'
+	ccm updateconf 'rpc_min_threads: 2' 'rpc_max_threads: 16'
+	ccm updateconf 'phi_convict_threshold: 10'
 	ccm start -v
 	ccm status
 	ccm node1 nodetool status
