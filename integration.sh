00e81f09cfd692e2045e9a96d0d0de8d2ae879a4
Chris Bannister <c.bannister@gmail.com>
2015-03-13 12:27:54 +0000

    check limits and user

--- integration.sh
+++ integration.sh
@@ -6,6 +6,9 @@ function run_tests() {
 	local clusterSize=3
 	local version=$1
 
+	ulimit -a
+	whoami
+
 	ccm create test -v binary:$version -n $clusterSize -d --vnodes
 
 	ccm updateconf 'client_encryption_options.enabled: true' 'client_encryption_options.keystore: testdata/pki/.keystore' 'client_encryption_options.keystore_password: cassandra' 'client_encryption_options.require_client_auth: true' 'client_encryption_options.truststore: testdata/pki/.truststore' 'client_encryption_options.truststore_password: cassandra' 'concurrent_reads: 2' 'concurrent_writes: 2' 'rpc_server_type: sync' 'rpc_min_threads: 2' 'rpc_max_threads: 2' 'write_request_timeout_in_ms: 5000' 'read_request_timeout_in_ms: 5000'
