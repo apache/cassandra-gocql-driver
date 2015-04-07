0894974fa73fa5e16b7e1183a08a6658c2f36bca
Ben Hood <0x6e6562@gmail.com>
2015-04-07 16:25:57 +0100

    Add the runssl flag

--- integration.sh
+++ integration.sh
@@ -53,7 +53,7 @@ function run_tests() {
 
 	if [ "$auth" = true ]
     then
-    	go test -v . -timeout 15s -run=TestAuthentication -tags integration -runauth -proto=$proto -cluster=$(ccm liveset) -clusterSize=$clusterSize -autowait=1000ms
+    	go test -v . -timeout 15s -run=TestAuthentication -tags integration -runssl -runauth -proto=$proto -cluster=$(ccm liveset) -clusterSize=$clusterSize -autowait=1000ms
 	else
 		go test -timeout 5m -tags integration -cover -v -runssl -proto=$proto -rf=3 -cluster=$(ccm liveset) -clusterSize=$clusterSize -autowait=2000ms -compressor=snappy ./... | tee results.txt
 
