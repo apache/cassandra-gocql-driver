94f8614b245fd3a0e3d2d9ca892ec4892c61877d
Chris Bannister <c.bannister@gmail.com>
2015-12-25 20:36:27 +0000

    store the hostinfo on the conn it is dialed

--- conn.go
+++ conn.go
@@ -131,6 +131,7 @@ type Conn struct {
 	version         uint8
 	currentKeyspace string
 	started         bool
+	host            *HostInfo
 
 	session *Session
 
@@ -190,6 +191,13 @@ func Connect(addr string, cfg *ConnConfig, errorHandler ConnErrorHandler, sessio
 		session:      session,
 		streams:      streams.New(cfg.ProtoVersion),
 	}
+
+	host, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+	c.host = session.ring.getHost(host)
 
 	if cfg.Keepalive > 0 {
 		c.setKeepalive(cfg.Keepalive)
