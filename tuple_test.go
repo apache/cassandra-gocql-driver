c711a53bb205d3bbedcf116d4fd0412614168724
Chris Bannister <c.bannister@gmail.com>
2016-01-10 19:55:16 +0000

    correctly handle tuples in MapScan
    Tuples are handled specially in Scan and as such each element in the
    tuple must be treated as a single column. MapScan now generates one
    element in the map for each element in the tuple, these can be accessed
    by name using the new TupleColumnName function.
    
    fixes #561

--- tuple_test.go
+++ tuple_test.go
@@ -49,3 +49,31 @@ func TestTupleSimple(t *testing.T) {
 		t.Errorf("expected to get coord.y=-100 got: %v", coord.y)
 	}
 }
+
+func TestTupleMapScan(t *testing.T) {
+	if *flagProto < protoVersion3 {
+		t.Skip("tuple types are only available of proto>=3")
+	}
+
+	session := createSession(t)
+	defer session.Close()
+
+	err := createTable(session, `CREATE TABLE gocql_test.tuple_map_scan(
+		id int,
+		val frozen<tuple<int, int>>,
+
+		primary key(id))`)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := session.Query(`INSERT INTO tuple_map_scan (id, val) VALUES (1, (1, 2));`).Exec(); err != nil {
+		t.Fatal(err)
+	}
+
+	m := make(map[string]interface{})
+	err = session.Query(`SELECT * FROM tuple_map_scan`).MapScan(m)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
