a2034c70c18f052b76c370eeb9c143cf1eaa3b19
Ian Lozinski <iml@uber.com>
2016-05-17 19:39:47 -0700

    Fix session_test

--- session_test.go
+++ session_test.go
@@ -12,9 +12,10 @@ func TestSessionAPI(t *testing.T) {
 	cfg := &ClusterConfig{}
 
 	s := &Session{
-		cfg:    *cfg,
-		cons:   Quorum,
-		policy: RoundRobinHostPolicy(),
+		cfg:       *cfg,
+		cons:      Quorum,
+		policy:    RoundRobinHostPolicy(),
+		queryPool: &sync.Pool{New: func() interface{} { return new(Query) }},
 	}
 
 	s.pool = cfg.PoolConfig.buildPool(s)
@@ -163,8 +164,9 @@ func TestBatchBasicAPI(t *testing.T) {
 	cfg := &ClusterConfig{RetryPolicy: &SimpleRetryPolicy{NumRetries: 2}}
 
 	s := &Session{
-		cfg:  *cfg,
-		cons: Quorum,
+		cfg:       *cfg,
+		cons:      Quorum,
+		queryPool: &sync.Pool{New: func() interface{} { return new(Query) }},
 	}
 	defer s.Close()
 
