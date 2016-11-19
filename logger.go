55a0619f51bb5661d8b774925b521f9b5746d621
Vincent Rischmann <me@vrischmann.me>
2016-11-19 20:59:38 +0100

    by default forward the logs to the standard logger

--- logger.go
+++ logger.go
@@ -1,9 +1,6 @@
 package gocql
 
-import (
-	"io/ioutil"
-	"log"
-)
+import "log"
 
 type StdLogger interface {
 	Print(v ...interface{})
@@ -11,4 +8,10 @@ type StdLogger interface {
 	Println(v ...interface{})
 }
 
-var Logger StdLogger = log.New(ioutil.Discard, "", log.LstdFlags)
+type defaultLogger struct{}
+
+func (l *defaultLogger) Print(v ...interface{})                 { log.Print(v...) }
+func (l *defaultLogger) Printf(format string, v ...interface{}) { log.Printf(format, v...) }
+func (l *defaultLogger) Println(v ...interface{})               { log.Println(v...) }
+
+var Logger StdLogger = &defaultLogger{}
