apache/cassandra-gocql-driver

commit 2416cf340d32ee20794e739fa794968858295098

Consistency no panic (#983)
* frame: add marshaller and unmarshaller for Consistency and SerialConsistency

* authors: update

* frame: ParseConsistencyWrapper renamed to ParseConsistency and ParseConsistency renamed to MustParseConsistency

frame.go
@@ -196,45 +196,51 @@ func (c Consistency) String() string {
 	}
 }
 
-func ParseConsistency(s string) Consistency {
-	switch strings.ToUpper(s) {
-	case "ANY":
-		return Any
-	case "ONE":
-		return One
-	case "TWO":
-		return Two
-	case "THREE":
-		return Three
-	case "QUORUM":
-		return Quorum
-	case "ALL":
-		return All
-	case "LOCAL_QUORUM":
-		return LocalQuorum
-	case "EACH_QUORUM":
-		return EachQuorum
-	case "LOCAL_ONE":
-		return LocalOne
-	default:
-		panic("invalid consistency: " + s)
-	}
+func (c Consistency) MarshalText() (text []byte, err error) {
+	return []byte(c.String()), nil
 }
 
-// ParseConsistencyWrapper wraps gocql.ParseConsistency to provide an err
-// return instead of a panic
-func ParseConsistencyWrapper(s string) (consistency Consistency, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			var ok bool
-			err, ok = r.(error)
-			if !ok {
-				err = fmt.Errorf("ParseConsistencyWrapper: %v", r)
-			}
-		}
-	}()
-	consistency = ParseConsistency(s)
-	return consistency, nil
+func (c *Consistency) UnmarshalText(text []byte) error {
+	switch string(text) {
+	case "ANY":
+		*c = Any
+	case "ONE":
+		*c = One
+	case "TWO":
+		*c = Two
+	case "THREE":
+		*c = Three
+	case "QUORUM":
+		*c = Quorum
+	case "ALL":
+		*c = All
+	case "LOCAL_QUORUM":
+		*c = LocalQuorum
+	case "EACH_QUORUM":
+		*c = EachQuorum
+	case "LOCAL_ONE":
+		*c = LocalOne
+	default:
+		return fmt.Errorf("invalid consistency %q", string(text))
+	}
+
+	return nil
+}
+
+func ParseConsistency(s string) (consistency Consistency, err error) {
+	err = consistency.UnmarshalText([]byte(strings.ToUpper(s)))
+	return
+}
+
+// ParseConsistencyWrapper is deprecated use ParseConsistency instead.
+var ParseConsistencyWrapper = ParseConsistency
+
+func MustParseConsistency(s string) Consistency {
+	c, err := ParseConsistency(s)
+	if err != nil {
+		panic(err)
+	}
+	return c
 }
 
 type SerialConsistency uint16
@@ -253,6 +259,23 @@ func (s SerialConsistency) String() string {
 	default:
 		return fmt.Sprintf("UNKNOWN_SERIAL_CONS_0x%x", uint16(s))
 	}
+}
+
+func (s SerialConsistency) MarshalText() (text []byte, err error) {
+	return []byte(s.String()), nil
+}
+
+func (s *SerialConsistency) UnmarshalText(text []byte) error {
+	switch string(text) {
+	case "SERIAL":
+		*s = Serial
+	case "LOCAL_SERIAL":
+		*s = LocalSerial
+	default:
+		return fmt.Errorf("invalid consistency %q", string(text))
+	}
+
+	return nil
 }
 
 const (

AUTHORS
@@ -94,4 +94,5 @@ Krishnanand Thommandra <devtkrishna@gmail.com>
 Blake Atkinson <me@blakeatkinson.com>
 Dharmendra Parsaila <d4dharmu@gmail.com>
 Nayef Ghattas <nayef.ghattas@datadoghq.com>
+Michał Matczuk <mmatczuk@gmail.com>
 Ben Krebsbach <ben.krebsbach@gmail.com>

frame_test.go
@@ -97,10 +97,3 @@ func TestFrameReadTooLong(t *testing.T) {
 		t.Fatalf("expected to get header %v got %v", opReady, head.op)
 	}
 }
-
-func TestParseConsistencyErrorInsteadOfPanic(t *testing.T) {
-	_, err := ParseConsistencyWrapper("TEST")
-	if err == nil {
-		t.Fatal("expected ParseConsistencyWrapper error got nil")
-	}
-}

commit 939af06b39e61c17b91b7c11720213246ece9b80

Fix breaking change introduced in #983

frame.go
@@ -227,20 +227,30 @@ func (c *Consistency) UnmarshalText(text []byte) error {
 	return nil
 }
 
-func ParseConsistency(s string) (consistency Consistency, err error) {
+func ParseConsistency(s string) Consistency {
+	var c Consistency
+	if err := c.UnmarshalText([]byte(strings.ToUpper(s))); err != nil {
+		panic(err)
+	}
+	return c
+}
+
+// ParseConsistencyWrapper wraps gocql.ParseConsistency to provide an err
+// return instead of a panic
+func ParseConsistencyWrapper(s string) (consistency Consistency, err error) {
 	err = consistency.UnmarshalText([]byte(strings.ToUpper(s)))
 	return
 }
 
-// ParseConsistencyWrapper is deprecated use ParseConsistency instead.
-var ParseConsistencyWrapper = ParseConsistency
-
-func MustParseConsistency(s string) Consistency {
-	c, err := ParseConsistency(s)
+// MustParseConsistency is the same as ParseConsistency except it returns
+// an error (never). It is kept here since breaking changes are not good.
+// DEPRECATED: use ParseConsistency if you want a panic on parse error.
+func MustParseConsistency(s string) (Consistency, error) {
+	c, err := ParseConsistencyWrapper(s)
 	if err != nil {
 		panic(err)
 	}
-	return c
+	return c, nil
 }
 
 type SerialConsistency uint16