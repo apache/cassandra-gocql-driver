2e9f2912ba58e37f34af3554659b576eac655760
Zhixin Wen <john.wenzhixin@hotmail.com>
2018-02-24 01:24:22 -0800

    fix memory leak issue (#1069)

--- conn.go
+++ conn.go
@@ -710,10 +710,12 @@ func (c *Conn) sendFrame(ctx context.Context, call *callReq, timeoutCh <-chan ti
 	case c.frameWriteArgChan <- call:
 		return nil
 	case <-timeoutCh:
+		c.releaseStream(call.streamID)
 		close(call.timeout)
 		c.handleTimeout()
 		return ErrTimeoutNoResponse
 	case <-ctxDone:
+		c.releaseStream(call.streamID)
 		close(call.timeout)
 		return ctx.Err()
 	case <-c.quit:
