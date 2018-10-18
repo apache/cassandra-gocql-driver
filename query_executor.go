scylladb/gocql

hostConnPool: introduced ConnPicker interface to abstract pool storage
The hostConnPool logic around conns slice is moved to defaultConnPicker.

Co-authored-by: Henrik Johansson <henrik@scylladb.com>
Co-authored-by: Michał Matczuk <michal@scylladb.com>

query_executor.go
@@ -115,7 +115,7 @@ func (q *queryExecutor) run(qry ExecutableQuery, specWG *sync.WaitGroup, results
 			continue
 		}
 
-		conn := pool.Pick()
+		conn := pool.Pick(hostResponse.Token())
 		if conn == nil {
 			continue
 		}

apache/cassandra-gocql-driver

query_executor.go
@@ -117,7 +117,7 @@ func (q *queryExecutor) run(qry ExecutableQuery, specWG *sync.WaitGroup, results
 			continue
 		}
 
-		conn := pool.Pick()
+		conn := pool.Pick(selectedHost.Token())
 		if conn == nil {
 			selectedHost = hostIter()
 			continue