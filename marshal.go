dbf39544ac441745c59ae45b6f6ddab402a10e49
adrianwit <adrianwit@gmail.com>
2018-11-29 17:37:55 -0800

    updated marshaler to support float from int64, float64, timestamp from string to integrated with viant/endly e2e testing via dependencies https://github.com/viant/dsunit/tree/casandra and https://github.com/viant/dsc/tree/casandra

--- marshal.go
+++ marshal.go
@@ -937,6 +937,12 @@ func marshalFloat(info TypeInfo, value interface{}) ([]byte, error) {
 		return nil, nil
 	case float32:
 		return encInt(int32(math.Float32bits(v))), nil
+	case int64:
+		return encInt(int32(math.Float32bits(float32(v)))), nil
+	case float64:
+		f := float32(v)
+		return encInt(int32(math.Float32bits(f))), nil
+
 	}
 
 	if value == nil {
@@ -955,6 +961,7 @@ func unmarshalFloat(info TypeInfo, data []byte, value interface{}) error {
 	switch v := value.(type) {
 	case Unmarshaler:
 		return v.UnmarshalCQL(info, data)
+
 	case *float32:
 		*v = math.Float32frombits(uint32(decInt(data)))
 		return nil
@@ -1098,6 +1105,18 @@ func marshalTimestamp(info TypeInfo, value interface{}) ([]byte, error) {
 		return nil, nil
 	case int64:
 		return encBigInt(v), nil
+	case string:
+		defaultLayout := time.RFC3339
+		if len(v) < len(defaultLayout) {
+			defaultLayout = string(defaultLayout[:len(v)])
+		}
+		t, err := time.Parse(defaultLayout, v)
+		if err != nil {
+			return nil, err
+		}
+		x := int64(t.UTC().Unix()*1e3) + int64(t.UTC().Nanosecond()/1e6)
+		return encBigInt(x), nil
+
 	case time.Time:
 		if v.IsZero() {
 			return []byte{}, nil
