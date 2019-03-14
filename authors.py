d90a3e2cadb3ba8ea5213c9abbbc39c0721d5e5a
Martin Sucha <martin.sucha@kiwi.com>
2019-03-14 14:42:12 +0100

    Add a check in Travis to enforce AUTHORS file order

--- authors.py
+++ authors.py
@@ -2,10 +2,12 @@
 # Helper script for managing AUTHORS file
 import os
 import argparse
+import io
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('author', nargs='?')
+parser.add_argument('--check', action='store_true', help='Return 0 if no changes are required')
 
 args = parser.parse_args()
 
@@ -22,10 +24,23 @@ with open('AUTHORS', 'r', encoding='UTF-8') as f:
 if args.author:
     authors.append(args.author + '\n')
 
-with open('AUTHORS.tmp', 'w', encoding='UTF-8') as f:
+
+def write_authors(f):
     for line in preamble:
         f.write(line)
     for author in sorted(authors, key=str.casefold):
         f.write(author)
 
-os.rename('AUTHORS.tmp', 'AUTHORS')
+if args.check:
+    with open('AUTHORS', 'rb') as f:
+        original = f.read()
+    f = io.StringIO()
+    write_authors(f)
+    new = f.getvalue().encode('UTF-8')
+    if original != new:
+        print("Changes to AUTHORS file required")
+        raise SystemExit(1)
+else:
+    with open('AUTHORS.tmp', 'w', encoding='UTF-8') as f:
+        write_authors(f)
+    os.rename('AUTHORS.tmp', 'AUTHORS')
