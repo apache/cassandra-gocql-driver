8d5817dd0ee5b189f3fad86eed305f3c621e6f93
beltran <jaumemarhuenda@gmail.com>
2020-05-21 05:38:37 +0200

    add types metadata

--- metadata.go
+++ metadata.go
@@ -25,6 +25,7 @@ type KeyspaceMetadata struct {
 	// Deprecated: use the MaterializedViews field instead.
 	Views             map[string]*ViewMetadata
 	MaterializedViews map[string]*MaterializedViewMetadata
+	Types             map[string]*TypeMetadata
 }
 
 // schema metadata for a table (a.k.a. column family)
@@ -118,6 +119,13 @@ type MaterializedViewMetadata struct {
 	SpeculativeRetry        string
 
 	baseTableName string
+}
+
+type TypeMetadata struct {
+	Keyspace   string
+	Name       string
+	FieldNames []string
+	FieldTypes []TypeInfo
 }
 
 // the ordering of the column with regard to its comparator
@@ -324,6 +332,19 @@ func compileMetadata(
 	keyspace.Views = make(map[string]*ViewMetadata, len(views))
 	for i := range views {
 		keyspace.Views[views[i].Name] = &views[i]
+	}
+	// Views currently holds the types and hasn't been deleted for backward compatibility issues.
+	// That's why it's ok to copy Views into Types in this case. For the real Views use MaterializedViews.
+	types := make([]TypeMetadata, len(views))
+	for i := range views {
+		types[i].Keyspace = views[i].Keyspace
+		types[i].Name = views[i].Name
+		types[i].FieldNames = views[i].FieldNames
+		types[i].FieldTypes = views[i].FieldTypes
+	}
+	keyspace.Types = make(map[string]*TypeMetadata, len(views))
+	for i := range types {
+		keyspace.Types[types[i].Name] = &types[i]
 	}
 	keyspace.MaterializedViews = make(map[string]*MaterializedViewMetadata, len(materializedViews))
 	for _, materializedView := range materializedViews {
