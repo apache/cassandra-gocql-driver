apache/cassandra-gocql-driver

commit 27ecec82288579a021a926bbfc6b12d1a31a62d8

Remove dead code (#1504)
This code is not used anywhere.

conn.go
@@ -176,8 +176,6 @@ type Conn struct {
 	closed int32
 	ctx    context.Context
 	cancel context.CancelFunc
-
-	timeouts int64
 }
 
 // connect establishes a connection to a Cassandra node using session's connection config.

common_test.go
@@ -153,26 +153,6 @@ func createSession(tb testing.TB, opts ...func(config *ClusterConfig)) *Session 
 	return createSessionFromCluster(cluster, tb)
 }
 
-// createTestSession is hopefully moderately useful in actual unit tests
-func createTestSession() *Session {
-	config := NewCluster()
-	config.NumConns = 1
-	config.Timeout = 0
-	config.DisableInitialHostLookup = true
-	config.IgnorePeerAddr = true
-	config.PoolConfig.HostSelectionPolicy = RoundRobinHostPolicy()
-	session := &Session{
-		cfg: *config,
-		connCfg: &ConnConfig{
-			Timeout:   10 * time.Millisecond,
-			Keepalive: 0,
-		},
-		policy: config.PoolConfig.HostSelectionPolicy,
-	}
-	session.pool = config.PoolConfig.buildPool(session)
-	return session
-}
-
 func createViews(t *testing.T, session *Session) {
 	if err := session.Query(`
 		CREATE TYPE IF NOT EXISTS gocql_test.basicView (
@@ -291,12 +271,5 @@ func assertNil(t *testing.T, description string, actual interface{}) {
 	t.Helper()
 	if actual != nil {
 		t.Fatalf("expected %s to be (nil) but was (%+v) instead", description, actual)
-	}
-}
-
-func assertNotNil(t *testing.T, description string, actual interface{}) {
-	t.Helper()
-	if actual == nil {
-		t.Fatalf("expected %s not to be (nil)", description)
 	}
 }

conn_test.go
@@ -1078,31 +1078,17 @@ type TestServer struct {
 	t                testing.TB
 	listen           net.Listener
 	nKillReq         int64
-	compressor       Compressor
 
 	protocol   byte
 	headerSize int
 	ctx        context.Context
 	cancel     context.CancelFunc
 
-	quit   chan struct{}
 	mu     sync.Mutex
 	closed bool
 
 	// onRecv is a hook point for tests, called in receive loop.
 	onRecv func(*framer)
-}
-
-func (srv *TestServer) session() (*Session, error) {
-	return testCluster(protoVersion(srv.protocol), srv.Address).CreateSession()
-}
-
-func (srv *TestServer) host() *HostInfo {
-	hosts, err := hostInfo(srv.Address, 9042)
-	if err != nil {
-		srv.t.Fatal(err)
-	}
-	return hosts[0]
 }
 
 func (srv *TestServer) closeWatch() {

connectionpool.go
@@ -252,12 +252,6 @@ func (p *policyConnPool) removeHost(ip net.IP) {
 	go pool.Close()
 }
 
-func (p *policyConnPool) hostUp(host *HostInfo) {
-	// TODO(zariel): have a set of up hosts and down hosts, we can internally
-	// detect down hosts, then try to reconnect to them.
-	p.addHost(host)
-}
-
 func (p *policyConnPool) hostDown(ip net.IP) {
 	// TODO(zariel): mark host as down so we can try to connect to it later, for
 	// now just treat it has removed.

frame.go
@@ -311,24 +311,8 @@ var (
 
 const maxFrameHeaderSize = 9
 
-func writeInt(p []byte, n int32) {
-	p[0] = byte(n >> 24)
-	p[1] = byte(n >> 16)
-	p[2] = byte(n >> 8)
-	p[3] = byte(n)
-}
-
 func readInt(p []byte) int32 {
 	return int32(p[0])<<24 | int32(p[1])<<16 | int32(p[2])<<8 | int32(p[3])
-}
-
-func writeShort(p []byte, n uint16) {
-	p[0] = byte(n >> 8)
-	p[1] = byte(n)
-}
-
-func readShort(p []byte) uint16 {
-	return uint16(p[0])<<8 | uint16(p[1])
 }
 
 type frameHeader struct {
@@ -1792,16 +1776,6 @@ func (f *framer) readShort() (n uint16) {
 	return
 }
 
-func (f *framer) readLong() (n int64) {
-	if len(f.rbuf) < 8 {
-		panic(fmt.Errorf("not enough bytes in buffer to read long require 8 got: %d", len(f.rbuf)))
-	}
-	n = int64(f.rbuf[0])<<56 | int64(f.rbuf[1])<<48 | int64(f.rbuf[2])<<40 | int64(f.rbuf[3])<<32 |
-		int64(f.rbuf[4])<<24 | int64(f.rbuf[5])<<16 | int64(f.rbuf[6])<<8 | int64(f.rbuf[7])
-	f.rbuf = f.rbuf[8:]
-	return
-}
-
 func (f *framer) readString() (s string) {
 	size := f.readShort()
 
@@ -1915,19 +1889,6 @@ func (f *framer) readConsistency() Consistency {
 	return Consistency(f.readShort())
 }
 
-func (f *framer) readStringMap() map[string]string {
-	size := f.readShort()
-	m := make(map[string]string, size)
-
-	for i := 0; i < int(size); i++ {
-		k := f.readString()
-		v := f.readString()
-		m[k] = v
-	}
-
-	return m
-}
-
 func (f *framer) readBytesMap() map[string][]byte {
 	size := f.readShort()
 	m := make(map[string][]byte, size)
@@ -2037,10 +1998,6 @@ func (f *framer) writeLongString(s string) {
 	f.wbuf = append(f.wbuf, s...)
 }
 
-func (f *framer) writeUUID(u *UUID) {
-	f.wbuf = append(f.wbuf, u[:]...)
-}
-
 func (f *framer) writeStringList(l []string) {
 	f.writeShort(uint16(len(l)))
 	for _, s := range l {
@@ -2071,18 +2028,6 @@ func (f *framer) writeBytes(p []byte) {
 func (f *framer) writeShortBytes(p []byte) {
 	f.writeShort(uint16(len(p)))
 	f.wbuf = append(f.wbuf, p...)
-}
-
-func (f *framer) writeInet(ip net.IP, port int) {
-	f.wbuf = append(f.wbuf,
-		byte(len(ip)),
-	)
-
-	f.wbuf = append(f.wbuf,
-		[]byte(ip)...,
-	)
-
-	f.writeInt(int32(port))
 }
 
 func (f *framer) writeConsistency(cons Consistency) {

helpers.go
@@ -270,15 +270,6 @@ func getApacheCassandraType(class string) Type {
 	}
 }
 
-func typeCanBeNull(typ TypeInfo) bool {
-	switch typ.(type) {
-	case CollectionType, UDTTypeInfo, TupleTypeInfo:
-		return false
-	}
-
-	return true
-}
-
 func (r *RowData) rowMap(m map[string]interface{}) {
 	for i, column := range r.Columns {
 		val := dereference(r.Values[i])

host_source.go
@@ -147,13 +147,6 @@ func (h *HostInfo) Peer() net.IP {
 	return h.peer
 }
 
-func (h *HostInfo) setPeer(peer net.IP) *HostInfo {
-	h.mu.Lock()
-	defer h.mu.Unlock()
-	h.peer = peer
-	return h
-}
-
 func (h *HostInfo) invalidConnectAddr() bool {
 	h.mu.RLock()
 	defer h.mu.RUnlock()
@@ -233,13 +226,6 @@ func (h *HostInfo) DataCenter() string {
 	return dc
 }
 
-func (h *HostInfo) setDataCenter(dataCenter string) *HostInfo {
-	h.mu.Lock()
-	defer h.mu.Unlock()
-	h.dataCenter = dataCenter
-	return h
-}
-
 func (h *HostInfo) Rack() string {
 	h.mu.RLock()
 	rack := h.rack
@@ -247,24 +233,10 @@ func (h *HostInfo) Rack() string {
 	return rack
 }
 
-func (h *HostInfo) setRack(rack string) *HostInfo {
-	h.mu.Lock()
-	defer h.mu.Unlock()
-	h.rack = rack
-	return h
-}
-
 func (h *HostInfo) HostID() string {
 	h.mu.RLock()
 	defer h.mu.RUnlock()
 	return h.hostId
-}
-
-func (h *HostInfo) setHostID(hostID string) *HostInfo {
-	h.mu.Lock()
-	defer h.mu.Unlock()
-	h.hostId = hostID
-	return h
 }
 
 func (h *HostInfo) WorkLoad() string {
@@ -303,13 +275,6 @@ func (h *HostInfo) Version() cassVersion {
 	return h.version
 }
 
-func (h *HostInfo) setVersion(major, minor, patch int) *HostInfo {
-	h.mu.Lock()
-	defer h.mu.Unlock()
-	h.version = cassVersion{major, minor, patch}
-	return h
-}
-
 func (h *HostInfo) State() nodeState {
 	h.mu.RLock()
 	defer h.mu.RUnlock()
@@ -329,24 +294,10 @@ func (h *HostInfo) Tokens() []string {
 	return h.tokens
 }
 
-func (h *HostInfo) setTokens(tokens []string) *HostInfo {
-	h.mu.Lock()
-	defer h.mu.Unlock()
-	h.tokens = tokens
-	return h
-}
-
 func (h *HostInfo) Port() int {
 	h.mu.RLock()
 	defer h.mu.RUnlock()
 	return h.port
-}
-
-func (h *HostInfo) setPort(port int) *HostInfo {
-	h.mu.Lock()
-	defer h.mu.Unlock()
-	h.port = port
-	return h
 }
 
 func (h *HostInfo) update(from *HostInfo) {

internal/lru/lru_test.go
@@ -21,16 +21,6 @@ import (
 	"testing"
 )
 
-type simpleStruct struct {
-	int
-	string
-}
-
-type complexStruct struct {
-	int
-	simpleStruct
-}
-
 var getTests = []struct {
 	name       string
 	keyToAdd   string

commit 1418e1105e720e09744dd50f9b21aed3911bf434

Fix build
This is a regression from 27ecec82288579a021a926bbfc6b12d1a31a62d8
that removed c.timeouts even when it should not.

conn.go
@@ -176,6 +176,8 @@ type Conn struct {
 	closed int32
 	ctx    context.Context
 	cancel context.CancelFunc
+
+	timeouts int64
 }
 
 // connect establishes a connection to a Cassandra node using session's connection config.