3a32123c5172809f797b84cdef955db675629574
Martin Sucha <martin.sucha@kiwi.com>
2021-06-21 14:58:16 +0200

    Install psutil to fix CI
    ccm requires psutil

--- install_test_deps.sh
+++ install_test_deps.sh
@@ -3,7 +3,7 @@
 # This is not supposed to be an error-prone script; just a convenience.
 
 # Install CCM
-pip install --user cql PyYAML six
+pip install --user cql PyYAML six psutil
 git clone https://github.com/pcmanus/ccm.git
 pushd ccm
 ./setup.py install --user
