68445fa32afa24684c4a8ab03da2bc37fd8fe5f2
Eric Evans <eevans@wikimedia.org>
2022-10-28 16:15:04 -0500

    Upgrade pierrec/lz4 to 4.1.17
    There are bug fixes in the new version.
    
    The `lz4.UncompressBlock(...)` was changed in 4.1.14 to be compatible
    with the asm decoders.  As a result, `TestLZ4Compressor` was updated
    to pass under the new semantics (see: pierrec/lz4#160 and
    pierrec/lz4#163)

--- lz4/go.mod
+++ lz4/go.mod
@@ -3,6 +3,6 @@ module github.com/gocql/gocql/lz4
 go 1.16
 
 require (
-	github.com/pierrec/lz4/v4 v4.1.8
+	github.com/pierrec/lz4/v4 v4.1.17
 	github.com/stretchr/testify v1.7.0
 )
--- lz4/lz4_test.go
+++ lz4/lz4_test.go
@@ -13,9 +13,6 @@ func TestLZ4Compressor(t *testing.T) {
 	_, err := c.Decode([]byte{0, 1, 2})
 	require.EqualError(t, err, "cassandra lz4 block size should be >4, got=3")
 
-	_, err = c.Decode([]byte{0, 1, 2, 4, 5})
-	require.EqualError(t, err, "lz4: invalid source or destination buffer too short")
-
 	// If uncompressed size is zero then nothing is decoded even if present.
 	decoded, err := c.Decode([]byte{0, 0, 0, 0, 5, 7, 8})
 	require.NoError(t, err)
