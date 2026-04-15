1b541aed12d31dfe109f023a0ef70c7f61bd2ba1
Bohdan Siryk <bohdan.siryk@netapp.com>
2026-04-15 13:40:35 +0300

    clarify warn log

--- session.go
+++ session.go
@@ -152,7 +152,7 @@ func NewSession(cfg ClusterConfig) (*Session, error) {
 	logger := cfg.newLogger()
 	if cfg.Encoding.EncodeNilMapAsInitilizedUDT && !cfg.Encoding.SuppressEncodeNilMapAsInitilizedUDTWarning {
 		logger.Warning("EncodingConfig.EncodeNilMapAsInitilizedUDT is enabled. " +
-			"This is a backward compatibility option for applications that might rely old behavior of MapScan when scanning UDTs. " +
+			"This is a backward compatibility option for applications that might rely old behavior encoding nil maps for UDTs. " +
 			"Please consider using the new encoding behavior instead if possible. " +
 			"To suppress this warning, set EncodingConfig.SuppressEncodeNilMapAsInitilizedUDTWarning to true. " +
 			"Follow https://issues.apache.org/jira/browse/CASSGO-118 for more details.")
